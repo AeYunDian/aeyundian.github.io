a7d38e03d9182590790bccc847d8a9e35f99660c
AeYunDian <zhanghaoyu19281@outlook.com>
2026-03-02 11:32:50 +0800

    Ver 59

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -13,6 +13,17 @@ export default defineUserConfig({
 
   theme,
 
+  chainWebpack(config) {
+    // 修改 css-loader 选项，不对以 '/' 开头的 url 进行解析
+    config.module
+      .rule('scss')
+      .oneOf('normal')
+      .use('css-loader')
+      .tap(options => {
+        options.url = { filter: (url) => !url.startsWith('/') }
+        return options
+      })
+  }
   // 和 PWA 一起启用
   // shouldPrefetch: false,
 });
