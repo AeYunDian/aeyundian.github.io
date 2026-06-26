001ede6e1fd872d0c8e2d9b15fe66b1533304674
AeYunDian <zhanghaoyu19281@outlook.com>
2026-06-26 21:21:13 +0800

    modified:   worker/index.js

--- worker/index.js
+++ worker/index.js
@@ -1,8 +1,18 @@
+const mobileRegex = /android|iphone|ipad|ipod|blackberry|iemobile|opera mini|mobile|windows phone|phone|webos|kindle|tablet/i;
+
 export default {
     async fetch(request, env) {
         const url = new URL(request.url);
+        const path = url.pathname;
+        const userAgent = request.headers.get('User-Agent') || '';
+        const platform = request.headers.get('sec-ch-ua-platform') || '';
+        const hostname = url.hostname;
+        const cookie = request.headers.get('Cookie') || '';
+        const isWechat = !!userAgent.match(/MicroMessenger/i);
+        const clientIP = request.headers.get('CF-Connecting-IP');
+        const isMobile = mobileRegex.test(userAgent) || false;
 
-        if (url.protocol === 'http:' && !request.headers.get('User-Agent')?.includes('MSIE')) {
+        if (path === 'http:' && !request.headers.get('User-Agent')?.includes('MSIE')) {
             const newUrl = new URL(request.url);
             newUrl.protocol = 'https:';
             // 如果原端口是 80，则移除（使用 HTTPS 默认 443）
@@ -15,14 +25,22 @@ export default {
             });
         }
 
-        if (url.pathname === '/我们毕业啦.html' || url.pathname === '/我们毕业啦') {
-            return new Response(null, {
-                status: 301,
-                headers: { 'Location': 'https://net.undz.cn/static/mp4/9e7e0f7e8a3f752c47bf759d7f1f606f.mp4' }
-            });
+        if (path === '/我们毕业啦.html' || path === '/我们毕业啦') {
+            if (isMobile) {
+                return new Response(null, {
+                    status: 301,
+                    headers: { 'Location': 'https://net.undz.cn/static/mp4/9e7e0f7e8a3f752c47bf759d7f1f606f.mp4' }
+                });
+            } else {
+                return new Response(null, {
+                    status: 301,
+                    headers: { 'Location': 'https://www.bilibili.com/video/BV1GJ411x7h7/' }
+                });
+            }
+
         }
 
-        if (url.pathname.startsWith("/api/")) {
+        if (path.startsWith("/api/")) {
             return new Response(JSON.stringify({ name: "Cloudflare" }), {
                 headers: { "Content-Type": "application/json" },
             });
