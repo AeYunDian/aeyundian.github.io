630bd591952e96de47501474abebe4419922d63b
AeYunDian <zhanghaoyu19281@outlook.com>
2026-06-26 21:28:17 +0800

    modified:   worker/index.js

--- worker/index.js
+++ worker/index.js
@@ -12,7 +12,7 @@ export default {
         const clientIP = request.headers.get('CF-Connecting-IP');
         const isMobile = mobileRegex.test(userAgent) || false;
 
-        if (path === 'http:' && !request.headers.get('User-Agent')?.includes('MSIE')) {
+        if (url.protocol === 'http:' && !request.headers.get('User-Agent')?.includes('MSIE')) {
             const newUrl = new URL(request.url);
             newUrl.protocol = 'https:';
             // 如果原端口是 80，则移除（使用 HTTPS 默认 443）
